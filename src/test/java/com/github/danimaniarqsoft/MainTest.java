6ab9c26be0a7f6a6b4ab78487b145f9ca3d86c1b
daniel <daniel.cortes.pichardo@gmail.com>
2024-06-25 18:09:46 -0600

    chores

--- src/test/java/com/github/danimaniarqsoft/MainTest.java
+++ src/test/java/com/github/danimaniarqsoft/MainTest.java
@@ -3,19 +3,31 @@ package com.github.danimaniarqsoft;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 class MainTest {
 
   private Main cut;
 
   @BeforeEach
   void setUp() {
+    log.info("Hola mundo");
     this.cut = new Main();
   }
 
   @Test
   void firstTest() {
+    String input = "duke";
+
+    String result = cut.format(input);
+
+    assertEquals("DUKE", result);
+  }
+  @Test
+  void secondTest() {
     String input = "duke";
 
     String result = cut.format(input);
