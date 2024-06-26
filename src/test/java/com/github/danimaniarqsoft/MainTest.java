02e22e98bd5a1b965b6a26da8f8d3f36f5c459df
daniel <daniel.cortes.pichardo@gmail.com>
2024-06-25 18:45:54 -0600

    updating log4j properties

--- src/test/java/com/github/danimaniarqsoft/MainTest.java
+++ src/test/java/com/github/danimaniarqsoft/MainTest.java
@@ -7,31 +7,50 @@ import lombok.extern.slf4j.Slf4j;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
 @Slf4j
 class MainTest {
 
   private Main cut;
 
+  @BeforeAll
+  static void beforeAll() {
+    log.info("Before all");
+  }
+
   @BeforeEach
   void setUp() {
-    log.info("Hola mundo");
+    log.info("beforeEach");
     this.cut = new Main();
   }
 
   @Test
   void firstTest() {
+    log.info("firstTest");
     String input = "duke";
-
     String result = cut.format(input);
-
     assertEquals("DUKE", result);
   }
+
   @Test
   void secondTest() {
+    log.info("secondTest");
     String input = "duke";
-
     String result = cut.format(input);
-
     assertEquals("DUKE", result);
   }
+
+  @AfterEach
+  void afterEach() {
+    log.info("afterEach");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    log.info("After all");
+  }
+
 }
