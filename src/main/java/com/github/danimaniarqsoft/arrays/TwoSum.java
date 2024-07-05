d7461eaa7d3d64b1b8e9b208874c1034c31c0873
daniel <daniel.cortes.pichardo@gmail.com>
2024-07-05 09:18:40 -0600

    TwoSum

---
+++ src/main/java/com/github/danimaniarqsoft/arrays/TwoSum.java
@@ -0,0 +1,25 @@
+package com.github.danimaniarqsoft.arrays;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums.length < 2) {
+            return nums;
+        }
+
+        HashMap<Integer, Integer> maps = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (maps.get(diff) == null) {
+                maps.put(nums[i], i);
+            } else {
+                return new int[] { i, maps.get(diff) };
+            }
+        }
+
+        return new int[0];
+    }
+}
---
+++ src/test/java/com/github/danimaniarqsoft/arrays/TwoSumTest.java
@@ -0,0 +1,24 @@
+package com.github.danimaniarqsoft.arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.github.danimaniarqsoft.utils.ArrayPrint;
+import com.github.danimaniarqsoft.utils.StringArrayConverter;
+
+class TwoSumTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sum.csv", numLinesToSkip = 1)
+    void twoSum(
+            @ConvertWith(StringArrayConverter.class) int[] input,
+            int target,
+            @ConvertWith(StringArrayConverter.class) int[] expected) {
+        int[] result = new TwoSum().twoSum(input, target);
+        ArrayPrint.print(result);
+        assertArrayEquals(expected, result);
+    }
+}
