8779efca7dfcdd7d25eaae101ebf1df1f2fbc9d5
daniel <daniel.cortes.pichardo@gmail.com>
2024-07-09 15:46:13 -0600

    First recurring character

--- src/main/java/com/github/danimaniarqsoft/hashtables/FirstRecurringCharacter.java
+++ src/main/java/com/github/danimaniarqsoft/hashtables/FirstRecurringCharacter.java
@@ -1,43 +1,22 @@
 package com.github.danimaniarqsoft.hashtables;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class FirstRecurringCharacter {
 
     public int process(int[] array) {
-        HashMap<Integer, Integer[]> map = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            Integer[] pair = map.get(array[i]);
-            if (pair == null) {
-                map.put(array[i], new Integer[] { 1, i });
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int value : array) {
+            if (map.get(value) != null) {
+                return value;
             } else {
-                pair[0] = pair[0] + 1; // count
-                pair[1] = i; // last index
-                map.put(array[i], pair);
+                map.put(value, true);
             }
         }
 
-        Integer currentMaxValue = null;
-        Integer[] currentMax = null;
+        return -1;
 
-        for (Map.Entry<Integer, Integer[]> entry : map.entrySet()) {
-
-            if (entry.getValue()[0] > 1
-                    && (currentMax == null || (entry.getValue()[0] > currentMax[0])
-                            || (entry.getValue()[0] == currentMax[0]
-                                    && entry.getValue()[1] < currentMax[1]))) {
-                currentMax = entry.getValue();
-                currentMaxValue = entry.getKey();
-
-            }
-        }
-
-        if (currentMax == null) {
-            return -1;
-        } else {
-            return currentMaxValue;
-        }
     }
 
 }
